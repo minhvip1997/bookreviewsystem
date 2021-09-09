76a8e2365ec45022509344053c6f0d2771cd9be3
= <tnminh1997123@gmail.com>
2021-09-09 10:36:49 +0700

    fix bug

--- config/database.php
+++ config/database.php
@@ -46,8 +46,8 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '8081'),
             'database' => env('DB_DATABASE', 'booktest'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'minhvip1997'),
