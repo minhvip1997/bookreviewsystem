281c76578076121375d5608c5252aa06e0f5ddd6
= <tnminh1997123@gmail.com>
2021-09-10 07:26:40 +0700

    fix bug

--- database/migrations/2021_07_28_082045_comment.php
+++ database/migrations/2021_07_28_082045_comment.php
@@ -18,6 +18,7 @@ class Comment extends Migration
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('review_id');
             $table->longText('body');
+            $table->integer('approve');
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
