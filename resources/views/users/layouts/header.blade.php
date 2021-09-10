7d45dde48aaab30d8dc37f46ad345cef39697cc8
= <tnminh1997123@gmail.com>
2021-09-10 13:14:31 +0700

    fix bug

--- resources/views/users/layouts/header.blade.php
+++ resources/views/users/layouts/header.blade.php
@@ -33,5 +33,6 @@
    <link rel="stylesheet" href="{{ secure_asset('css/plugins.css') }}">
    <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
    <link rel="stylesheet" href="{{ secure_asset('css/custom.css') }}">
+   <link rel="stylesheet" href="{{ secure_asset('css/fontawesome.min.css') }}">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </header>
